75b51037edc8fc291a4163de73d87250b72edc3a
Rafael Avelino <avelino.rafael@escolar.ifrn.edu.br>
2024-12-04 11:17:05 -0300

    Correções no script

--- service.sh
+++ service.sh
@@ -16,9 +16,26 @@ create_service_from_image() {
 
 # Função para criar um serviço a partir de um Dockerfile
 create_service_from_dockerfile() {
+  cd ./$1
   docker build -t $1 .
-  docker run -d --name $1 $1
   echo "Serviço $1 criado a partir do Dockerfile."
+}
+
+# Função para dar Run no serviço
+run_service() {
+  case "$1" in
+    dns)   
+      docker run -d -p 53:53/tcp -p 53:53/udp --name $1 $1
+      echo "Serviço dns está rodando"
+    ;;
+    web)
+      docker run -d -p 80:80 --name $1 $1
+      echo "Serviço web está rodando"	
+    ;;
+    *)
+     echo "Não existe uma imagem com este nome"
+     echo "Tente utilizar 'dns' ou 'web'"
+  esac
 }
 
 # Função para iniciar um serviço
@@ -38,21 +55,19 @@ stop_service() {
 # Função para excluir um serviço
 remove_service() {
   check_service $1
-  docker rm $1
+  docker rm -f $1
   echo "Serviço $1 excluído."
 }
-
-# Verifica se foram passados os argumentos corretos
-if [ $# -ne 3 ]; then
-  echo "Uso: $0 <serviço> <ação> <imagem/Dockerfile>"
-  echo "Ações disponíveis: create, start, stop, remove"
-  exit 1
-fi
 
 # Executa a ação correspondente
 case "$2" in
   create)
-    if [[ -f Dockerfile ]]; then
+    if [ $# -ne 3 ]; then
+        echo "Uso: $0 <serviço> <ação> <imagem/Dockerfile>"
+        echo "Ações disponíveis: create, start, stop, remove"
+        exit 1
+
+    elif [[ -f ./$1/Dockerfile ]]; then
       create_service_from_dockerfile "$1"
     else
       create_service_from_image "$1" "$3"
@@ -67,8 +82,11 @@ case "$2" in
   remove)
     remove_service "$1"
     ;;
+  run)
+    run_service "$1"
+    ;;
   *)
-    echo "Ação inválida. Use 'create', 'start', 'stop' ou 'remove'."
+    echo "Ação inválida. Use 'create', 'start', 'stop' 'run' ou 'remove'."
     exit 1
     ;;
 esac
